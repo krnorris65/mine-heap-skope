d6c26b07f132489517e125176c97cae488b508f6
Kristen Norris <krnorris65@gmail.com>
2017-10-31 12:00:59 -0500

    initial set up of code for mine-heap skope

--- mine-heap-skope.js
+++ mine-heap-skope.js
@@ -1,87 +1,88 @@
-//per container: 565 kilograms of minerals & gems
-//heap skopes: 30 containers
-//process 5 kg at a time
+const gemHeapSkope = function () { // No parameter needed, resource contained inside
 
-
-const gemHeapSkope = function () {
-    //generator of gem containers
-    const gemContainerGenerator = function* () {
-        let currentContainer = 1
-        const maximumContainers = 30
-    
-        while (currentContainer <= maximumContainers) {
-            yield { "id": currentContainer, "type": "Mineral", "orders": [] }
-            currentContainer++
-        }
+    const GemMine1 = {
+        "Coal": 5302,
+        "Gold": 2775
     }
-    //instance of gem container generator
-    const gemContainerFactory = gemContainerGenerator()
-    
-    //two gem mines with two types of gems in each
-    const gemMine1 = {
-        "Coal": {
-            "kilograms": 5302
-        },
-        "Gold": {
-            "kilograms": 2775
-        }
+
+    const GemMine2 = {
+        "Iron": 3928,
+        "Copper": 901
     }
-    
-    const gemMine2 = {
-        "Iron": {
-            "kilograms": 3928
-        },
-        "Cooper": {
-            "kilograms": 901
+
+    /*
+    Hëap-skopes workshops can process 5 kilograms of a mineral with each work order. So every time the `process` function is invoked, subtract 5 from the amount of the requested mineral from the enclosed GemMine above.
+    */
+    return Object.create (null, {
+        "products": {
+            get: () => Object.keys(GemMine1, GemMine2) 
         }
+        // "process": {
+        //     value: requestedMineral => {
+
+        //         if ( /* 5kg, or more, of the mineral remaining? */ ) {
+        //             /*
+        //             You can reference the `GemMine` variable here because it lives in an outer scope: e.g. GemMine[requestedMineral].kilograms
+        //             */
+        //         }
+
+        //         return {
+        //             "mineral": requestedMineral,
+        //             "amount": 0 // Change this to the correct amount
+        //         }
+        //     }
+        // }
+    })
+}
+
+/*
+The SkopeManager variable represents the object with the `process` method on it.
+*/
+const SkopeManager = gemHeapSkope()
+
+/*
+Process the gems in any order you like until there none left in the gem mine.
+*/
+console.log(SkopeManager.products)
+
+//array that holds processed gems
+const processedGems = []
+
+
+//generator for 30 storage containers
+const gemContainerGenerator = function* () {
+    let containerId = 1
+    const maxContainers = 30
+
+    while (containerId <= maxContainers) {
+        yield { "id": containerId, "type": "Mineral", "orders": [] }
+        containerId++
     }
-    
-    //holds all the seperate gem containers
-    const gemContainers = []
-    
-    //current container
-    const currentContainer = gemContainerFactory.next().value
+}
 
-    return { 
-        "process": function (requestedMineral) {
-            let currentAmount = gemMine1[requestedMineral].kilograms
-
-            if(currentAmount >= 5) {
-                //if the current amount is greater than or equal to 5, then add 5kg of the mineral to the container
-                const addMineral = {
-                    "mineral": requestedMineral,
-                    "amount": 5
-                }
-                currentContainer.orders.push(addMineral)
-
-                //subtract 5 from current amount
-                currentAmount - 5
-                
-            } else if (currentAmount > 0) {
-                //if it is less than 5 then add the current amount but greater than 0
-                const addMineral = {
-                    "mineral": requestedMineral,
-                    "amount": currentAmount
-                }
-                currentContainer.orders.push(addMineral)
-            }
+//instance of gem container generator
+const gemContainerFactory = gemContainerGenerator()
 
 
-            return {
-                "mineral": requestedMineral,
-                "amount": //correct amount
-            }
-            
-            //once the the sum of amount in the orders array of current container reaches 565kg then move on to the next container
+//array that contains the gemContainers
+const gemContainers = []
 
-            //how many gem containers were created?
+//value of current container
+const currentContainer = gemContainerFactory.next().value
 
+// processedGems.forEach(
+//     currentGem => {
+//         if(currentGem){
+//             currentContainer.orders.push(currentGem) //add the current gem to the order array in the currentContainer
+//             let capcity = 565/(currentContainer.orders.length * 5) //determines if the container is full by dividing the maximum capicity of the container (565 gems) by the current amount in the container assuming each order has 5 gems
+//         }
+//         if(capcity === 1) { //if the container is full
+//             gemContainers.push(currentContainer) //add container to the gemContainers array
+//             currentContainer = gemContainerFactory.next().value //move on to the next container
+//         }
+//     }
+// )
 
-        }//end of process function
-
-    }//end of return1
-    
-   
-}//end of gemHeapSkope function
-
-console.log(gemHeapSkope())
+// if(currentContainer.orders.length > 0) { //even if the container isn't full
+//     gemContainers.push(currentContainer) //add container to the gemContainers array
+// }
